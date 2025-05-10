72dd0f468290921f4daa296c0f8c67c5dbf50eb5
Daryna <buriak.daryna@gmail.com>
2025-05-10 14:20:28 +0300

    Add remaining bug fixes

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -7,7 +7,7 @@ export function getEnvVar(name, defaultValue) {
 
   if (value) return value;
 
-  if (defaultValue) return defaultValue;
+  if (defaultValue !== undefined) return defaultValue;
 
   throw new Error(`Missing: process.env['${name}'].`);
 }
