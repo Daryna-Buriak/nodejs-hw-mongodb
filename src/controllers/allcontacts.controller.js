7537f67caa53ad128b2bf72ae138b67155b2830d
Daryna <buriak.daryna@gmail.com>
2025-05-23 19:05:29 +0300

    Add pagination

--- src/controllers/allcontacts.controller.js
+++ src/controllers/allcontacts.controller.js
@@ -1,8 +1,14 @@
 import { getAllContacts } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getAllContactsController = async (req, res) => {
   try {
-    const contacts = await getAllContacts();
+    const { page, perPage } = parsePaginationParams(req.query);
+    const contacts = await getAllContacts({
+      page,
+      perPage,
+    });
+
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
