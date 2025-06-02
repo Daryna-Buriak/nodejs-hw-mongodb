7907ca838fcb24016aed36d3f8ca2948b8c7366b
Daryna <buriak.daryna@gmail.com>
2025-06-02 21:03:41 +0300

    Add cookies

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
@@ -16,6 +17,7 @@ export const setupServer = async () => {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
