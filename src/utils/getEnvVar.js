64dbc74c5b7c2e19fa821a6703a4f6462efa9203
Daryna <buriak.daryna@gmail.com>
2025-06-09 13:36:54 +0300

    Add check for value

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -5,7 +5,7 @@ dotenv.config();
 export function getEnvVar(name, defaultValue) {
   const value = process.env[name];
 
-  if (value) return value;
+  if (value !== undefined) return value;
 
   if (defaultValue !== undefined) return defaultValue;
 
