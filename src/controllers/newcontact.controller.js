4850455640a455f6f9ebdcfa8f62b740f098ba02
Daryna <buriak.daryna@gmail.com>
2025-06-10 12:33:04 +0300

    Change route names and add photos to post

--- src/controllers/newcontact.controller.js
+++ src/controllers/newcontact.controller.js
@@ -1,7 +1,26 @@
 import { createContact } from '../services/contacts.js';
+import { getEnvVar } from '../utils/getEnvVar.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
 
 export const createContactController = async (req, res) => {
-  const contact = await createContact({ ...req.body, userId: req.user._id });
+  const photo = req.file;
+  let photoUrl;
+
+  if (photo) {
+    if (getEnvVar('UPLOAD_TO_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const contact = await createContact({
+    ...req.body,
+    userId: req.user._id,
+    photo: photoUrl,
+  });
+
   res.status(201).json({
     status: 201,
     message: 'Successfully created a contact!',
